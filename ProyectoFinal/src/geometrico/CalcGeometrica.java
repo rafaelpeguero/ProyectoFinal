8f16d66f8e88f1cdf5a8de9d8486ac43160e7b07
rafaelpeguero <48397649+rafaelpeguero@users.noreply.github.com>
2019-11-15 15:01:56 -0400

    Update CalcGeometrica.java
    Agregando la area del triangulo..

--- ProyectoFinal/src/geometrico/CalcGeometrica.java
+++ ProyectoFinal/src/geometrico/CalcGeometrica.java
@@ -8,6 +8,28 @@ public class CalcGeometrica {
 		// TODO Auto-generated constructor stub
 	}
 	
+	public double rectArea(double x1,double x2, double x3, double y1, double y2, double y3){//Vx3
+	 
+	}
+	public double cuadArea(double x1,double x2, double y1, double y2){//Vx2
+	
+	}
+	public double triaArea(double x1,double x2,double x3,double x4, double y1, double y2,double y3,double y4){//Vx4
+		//Área = 1/2 * | X1 * (Y2 - Y3) + X2 * (Y3 - Y1) + X3 * (Y1 - Y2) |”
+		double area = 0;
+		
+		area = Math.abs( x1*(y2-y3) + x2*(y3-y1) + x3*(y1-y2) )/2 ;
+		
+		return area;
+	
+	}
+	public double trapArea(double x1,double x2,double x3,double x4, double y1, double y2,double y3,double y4){//Vx4
+	
+	}
+	public double trapArea(double x1,double x2,double x3,double x4, double y1, double y2,double y3,double y4){//Vx4
+	
+	}
+	
 	
 
 }

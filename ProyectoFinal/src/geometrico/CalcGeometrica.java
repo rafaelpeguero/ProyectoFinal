d7d8415498ee85489df8f6ae33636dc55876ca88
RAFAELPEGUEROJR <RAFAELPEGUEROJR@DESKTOP-IH9FBTB>
2019-11-15 14:37:05 -0400

    Desarrollando calculadora general..

---
+++ ProyectoFinal/src/geometrico/CalcGeometrica.java
@@ -0,0 +1,13 @@
+package geometrico;
+
+public class CalcGeometrica {
+	
+	private double x1,x2,x3,x4,y1,y2,y3,y4;
+
+	public CalcGeometrica() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}

3a555eac07119dd1c1734eee85246b6da52963d8
RAFAELPEGUEROJR <RAFAELPEGUEROJR@DESKTOP-IH9FBTB>
2019-11-19 11:38:00 -0400

    Implementando nuevos metodos y restricciones en la captura de datos.

--- ProyectoFinal/src/Visual/ObjtHerramienta.java
+++ ProyectoFinal/src/Visual/ObjtHerramienta.java
@@ -76,12 +76,12 @@ public class ObjtHerramienta extends JFrame {
 		
 		JPanel panelDatos = new JPanel();
 		panelDatos.setBorder(new TitledBorder(null, "Datos", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panelDatos.setBounds(10, 19, 250, 185);
+		panelDatos.setBounds(10, 11, 272, 193);
 		contentPane.add(panelDatos);
 		panelDatos.setLayout(null);
 		
 		JLabel lblTipo = new JLabel("Tipo : ");
-		lblTipo.setBounds(10, 19, 31, 14);
+		lblTipo.setBounds(10, 30, 60, 14);
 		panelDatos.add(lblTipo);
 		
 		 cbxPrismas = new JComboBox<String>();
@@ -108,6 +108,16 @@ public class ObjtHerramienta extends JFrame {
 					txtVDi.setEnabled(true);
 					txtVDii.setEnabled(true);
 					
+					txtVAi.setText(null);
+					txtVAii.setText(null);
+					txtVBi.setText(null);
+					txtVBii.setText(null);
+					txtVCi.setText(null);
+					txtVCii.setText(null);
+					txtVDi.setText(null);
+					txtVDii.setText(null);
+					
+					
 				}if(cbxPrismas.getSelectedIndex()==3) {//RECTANGULO
 					txtVAi.setEnabled(true);
 					txtVAii.setEnabled(true);
@@ -116,6 +126,13 @@ public class ObjtHerramienta extends JFrame {
 					txtVCi.setEnabled(true);
 					txtVCii.setEnabled(true);
 					
+					txtVAi.setText(null);
+					txtVAii.setText(null);
+					txtVBi.setText(null);
+					txtVBii.setText(null);
+					txtVCi.setText(null);
+					txtVCii.setText(null);
+					
 				}if(cbxPrismas.getSelectedIndex()==4) {//TRIANGULO
 					txtVAi.setEnabled(true);
 					txtVAii.setEnabled(true);
@@ -123,6 +140,13 @@ public class ObjtHerramienta extends JFrame {
 					txtVBii.setEnabled(true);
 					txtVCi.setEnabled(true);
 					txtVCii.setEnabled(true);
+					
+					txtVAi.setText(null);
+					txtVAii.setText(null);
+					txtVBi.setText(null);
+					txtVBii.setText(null);
+					txtVCi.setText(null);
+					txtVCii.setText(null);
 					
 				}if(cbxPrismas.getSelectedIndex()==5) {//TRAPECIO
 					txtVAi.setEnabled(true);
@@ -134,166 +158,212 @@ public class ObjtHerramienta extends JFrame {
 					txtVDi.setEnabled(true);
 					txtVDii.setEnabled(true);
 					
+					txtVAi.setText(null);
+					txtVAii.setText(null);
+					txtVBi.setText(null);
+					txtVBii.setText(null);
+					txtVCi.setText(null);
+					txtVCii.setText(null);
+					txtVDi.setText(null);
+					txtVDii.setText(null);
+					
 				}
 				
 				
 			}
 		});
 		cbxPrismas.setModel(new DefaultComboBoxModel<String>(new String[] {"< Seleccione > ", "Cuadrado", "Rombo", "Rectangulo", "Triangulo", "Trapecio"}));
-		cbxPrismas.setBounds(80, 13, 150, 22);
+		cbxPrismas.setBounds(80, 24, 150, 22);
 		panelDatos.add(cbxPrismas);
 		
 		JLabel lblVerticeA = new JLabel("Vertice A :");
-		lblVerticeA.setBounds(10, 52, 62, 14);
+		lblVerticeA.setBounds(10, 63, 62, 14);
 		panelDatos.add(lblVerticeA);
 		
 		JLabel lblVericeB = new JLabel("Verice B : ");
-		lblVericeB.setBounds(10, 85, 62, 14);
+		lblVericeB.setBounds(10, 96, 62, 14);
 		panelDatos.add(lblVericeB);
 		
 		JLabel lblVerticeC = new JLabel("Vertice C :");
-		lblVerticeC.setBounds(10, 118, 62, 14);
+		lblVerticeC.setBounds(10, 129, 62, 14);
 		panelDatos.add(lblVerticeC);
 		
 		JLabel lblVerticeD = new JLabel("Vertice D :");
-		lblVerticeD.setBounds(10, 151, 62, 14);
+		lblVerticeD.setBounds(10, 162, 62, 14);
 		panelDatos.add(lblVerticeD);
 		
 		JLabel label = new JLabel(",");
-		label.setBounds(114, 61, 5, 14);
+		label.setBounds(114, 72, 5, 14);
 		panelDatos.add(label);
 		
 		JLabel label_1 = new JLabel(",");
-		label_1.setBounds(114, 89, 5, 14);
+		label_1.setBounds(114, 100, 5, 14);
 		panelDatos.add(label_1);
 		
 		JLabel label_2 = new JLabel(",");
-		label_2.setBounds(114, 124, 5, 14);
+		label_2.setBounds(114, 135, 5, 14);
 		panelDatos.add(label_2);
 		
 		JLabel label_3 = new JLabel(",");
-		label_3.setBounds(114, 157, 5, 14);
+		label_3.setBounds(114, 168, 5, 14);
 		panelDatos.add(label_3);
 		
 		txtVAi = new JTextField();
 		txtVAi.addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyTyped(KeyEvent e) {
-				char caracter = e.getKeyChar(); //captura
-				if( (caracter < '0') || (caracter > '9') ) {
-					e.consume();
-					JOptionPane.showMessageDialog(null, "Formato incorecto, solo se permiten numeros", "Aviso", JOptionPane.INFORMATION_MESSAGE);
-					
-				}
+				msgFormatoErroneo(e);
 			}
 		});
 		txtVAi.setText("x1");
 		txtVAi.setEnabled(false);
-		txtVAi.setBounds(80, 48, 31, 20);
+		txtVAi.setBounds(80, 59, 31, 20);
 		panelDatos.add(txtVAi);
 		txtVAi.setColumns(10);
 		
 		txtVAii = new JTextField();
+		txtVAii.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				msgFormatoErroneo(e);
+			}
+		});
 		txtVAii.setText("y1\r\n");
 		txtVAii.setEnabled(false);
-		txtVAii.setBounds(121, 48, 34, 20);
+		txtVAii.setBounds(121, 59, 34, 20);
 		panelDatos.add(txtVAii);
 		txtVAii.setColumns(10);
 		
 		txtVBi = new JTextField();
+		txtVBi.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				msgFormatoErroneo(e);
+			}
+		});
 		txtVBi.setText("x2");
 		txtVBi.setEnabled(false);
-		txtVBi.setBounds(80, 81, 31, 20);
+		txtVBi.setBounds(80, 92, 31, 20);
 		panelDatos.add(txtVBi);
 		txtVBi.setColumns(10);
 		
 		txtVBii = new JTextField();
+		txtVBii.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				msgFormatoErroneo(e);
+			}
+		});
 		txtVBii.setText("y2");
 		txtVBii.setEnabled(false);
-		txtVBii.setBounds(124, 81, 31, 20);
+		txtVBii.setBounds(124, 92, 31, 20);
 		panelDatos.add(txtVBii);
 		txtVBii.setColumns(10);
 		
 		txtVCi = new JTextField();
+		txtVCi.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				msgFormatoErroneo(e);
+			}
+		});
 		txtVCi.setText("x3");
 		txtVCi.setEnabled(false);
-		txtVCi.setBounds(80, 114, 31, 20);
+		txtVCi.setBounds(80, 125, 31, 20);
 		panelDatos.add(txtVCi);
 		txtVCi.setColumns(10);
 		
 		txtVCii = new JTextField();
+		txtVCii.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				msgFormatoErroneo(e);
+			}
+		});
 		txtVCii.setText("y3");
 		txtVCii.setEnabled(false);
-		txtVCii.setBounds(124, 114, 31, 20);
+		txtVCii.setBounds(124, 125, 31, 20);
 		panelDatos.add(txtVCii);
 		txtVCii.setColumns(10);
 		
 		txtVDi = new JTextField();
+		txtVDi.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				msgFormatoErroneo(e);
+			}
+		});
 		txtVDi.setText("x4");
 		txtVDi.setEnabled(false);
-		txtVDi.setBounds(80, 147, 31, 20);
+		txtVDi.setBounds(80, 158, 31, 20);
 		panelDatos.add(txtVDi);
 		txtVDi.setColumns(10);
 		
 		txtVDii = new JTextField();
+		txtVDii.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				msgFormatoErroneo(e);
+			}
+		});
 		txtVDii.setText("y4\r\n");
 		txtVDii.setEnabled(false);
-		txtVDii.setBounds(124, 147, 31, 20);
+		txtVDii.setBounds(124, 158, 31, 20);
 		panelDatos.add(txtVDii);
 		txtVDii.setColumns(10);
 		
 		JPanel panelFigura = new JPanel();
 		panelFigura.setBorder(new TitledBorder(null, "Figura 3D", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panelFigura.setBounds(270, 19, 335, 384);
+		panelFigura.setBounds(292, 19, 313, 384);
 		contentPane.add(panelFigura);
 		
 		JPanel panelResultados = new JPanel();
 		panelResultados.setBorder(new TitledBorder(null, "Resultados", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panelResultados.setBounds(10, 215, 250, 188);
+		panelResultados.setBounds(10, 210, 272, 193);
 		contentPane.add(panelResultados);
 		panelResultados.setLayout(null);
 		
-		JLabel lblVolumenPrisma = new JLabel("Volumen Prisma : ");
-		lblVolumenPrisma.setBounds(22, 66, 124, 14);
+		JLabel lblVolumenPrisma = new JLabel("Volumen Prisma (cm^3): ");
+		lblVolumenPrisma.setBounds(10, 68, 160, 20);
 		panelResultados.add(lblVolumenPrisma);
 		
-		JLabel lblVolumenBase = new JLabel("Volumen Base : ");
-		lblVolumenBase.setBounds(22, 26, 124, 14);
+		JLabel lblVolumenBase = new JLabel("Volumen Base (cm^3 ) : ");
+		lblVolumenBase.setBounds(10, 27, 160, 20);
 		panelResultados.add(lblVolumenBase);
 		
-		JLabel lblAreaLateralPrisma = new JLabel("Area Lateral Prisma :");
-		lblAreaLateralPrisma.setBounds(22, 106, 124, 14);
+		JLabel lblAreaLateralPrisma = new JLabel("Area Lateral Prisma (cm^2):");
+		lblAreaLateralPrisma.setBounds(10, 109, 160, 20);
 		panelResultados.add(lblAreaLateralPrisma);
 		
-		JLabel lblAreraTotalPrisma = new JLabel("Arera Total Prisma :");
-		lblAreraTotalPrisma.setBounds(22, 146, 124, 14);
+		JLabel lblAreraTotalPrisma = new JLabel("Arera Total Prisma (cm^2):");
+		lblAreraTotalPrisma.setBounds(10, 150, 160, 20);
 		panelResultados.add(lblAreraTotalPrisma);
 		
 		txtVbase = new JTextField();
 		txtVbase.setEditable(false);
 		txtVbase.setEnabled(false);
-		txtVbase.setBounds(147, 23, 70, 20);
+		txtVbase.setBounds(180, 27, 70, 20);
 		panelResultados.add(txtVbase);
 		txtVbase.setColumns(10);
 		
 		txtVLprisma = new JTextField();
 		txtVLprisma.setEditable(false);
 		txtVLprisma.setEnabled(false);
-		txtVLprisma.setBounds(147, 63, 70, 20);
+		txtVLprisma.setBounds(180, 68, 70, 20);
 		panelResultados.add(txtVLprisma);
 		txtVLprisma.setColumns(10);
 		
 		txtALprisma = new JTextField();
 		txtALprisma.setEditable(false);
 		txtALprisma.setEnabled(false);
-		txtALprisma.setBounds(147, 103, 70, 20);
+		txtALprisma.setBounds(180, 109, 70, 20);
 		panelResultados.add(txtALprisma);
 		txtALprisma.setColumns(10);
 		
 		txtATprisma = new JTextField();
 		txtATprisma.setEditable(false);
 		txtATprisma.setEnabled(false);
-		txtATprisma.setBounds(147, 143, 70, 20);
+		txtATprisma.setBounds(180, 150, 70, 20);
 		panelResultados.add(txtATprisma);
 		txtATprisma.setColumns(10);
 		
@@ -314,6 +384,7 @@ public class ObjtHerramienta extends JFrame {
 		JButton btnCalcular = new JButton("Calcular");
 		btnCalcular.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				
 			}
 		});
 		btnCalcular.setBounds(327, 11, 89, 23);
@@ -322,9 +393,64 @@ public class ObjtHerramienta extends JFrame {
 		JButton btnLimpiar = new JButton("Limpiar");
 		btnLimpiar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				txtVAi.setText(null);
+				txtVAii.setText(null);
+				txtVBi.setText(null);
+				txtVBii.setText(null);
+				txtVCi.setText(null);
+				txtVCii.setText(null);
+				txtVDi.setText(null);
+				txtVDii.setText(null);
+				
+				txtVbase.setText(null);
+				txtVLprisma.setText(null);
+				txtVbase.setText(null);
+				txtALprisma.setText(null);
+				txtATprisma.setText(null);
+				
+				
 			}
 		});
 		btnLimpiar.setBounds(426, 11, 89, 23);
 		panelBtns.add(btnLimpiar);
 	}
+	
+	
+	public void msgFormatoErroneo(KeyEvent e) {
+		char caracter = e.getKeyChar(); //captura
+		if( (caracter < '0') || (caracter > '9') ) {
+			e.consume();
+			JOptionPane.showMessageDialog(null, "Formato incorecto, solo se permiten numeros", "Aviso", JOptionPane.INFORMATION_MESSAGE);
+			}
+	}
+	public void calcular(String tipo ) {
+		
+		switch (tipo) {
+		case "Cuadrado" :{
+			
+		}
+		case "Rombo" :{
+			
+		}
+		case "Rectangulo" :{
+			
+		}
+		case "Triangulo" :{
+			
+		}
+		case "Trapecio" :{
+			
+		}
+		
+		default : {
+			JOptionPane.showMessageDialog(null, JOptionPane.ERROR_MESSAGE);
+		}
+		
+		}
+		
+		
+	}
+		
+	
+	
 }
